1463380f45163a9cbc384188615a54a3511577fa
Gustavo Espinosa <age.espinosa@gmail.com>
2019-06-29 11:41:38 -0300

    Escribiendo el show y create en el contoller backend

--- src/main/java/com/age/springbootbackendapirest/controllers/ClienteRestController.java
+++ src/main/java/com/age/springbootbackendapirest/controllers/ClienteRestController.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +26,19 @@ public class ClienteRestController {
 	public List<Cliente> list(){
 		return clienteService.findAll();
 	}
+	
+	@GetMapping("/clientes/id")
+	public Cliente show(@PathVariable Long id) {
+		return clienteService.findById(id);
+	}
+	
+	@PostMapping("/clientes")
+	public Cliente create(Cliente cliente) {
+		return clienteService.save(cliente);
+	}
 
+	@DeleteMapping("/clientes/id")
+	public void delete(@PathVariable Long id) {
+		 clienteService.delete(id);
+	}
 }
